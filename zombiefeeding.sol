926dc3512e6f071447968ac534eb6eba30da6e1c
Swanagan Ray <65131940+sray91@users.noreply.github.com>
2021-06-24 15:13:40 -0400

    Update zombiefeeding.sol

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -4,6 +4,11 @@ import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
-  // Start here
-
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]); // make sure current user is the owner
+    Zombie storage myZombie = zombies[_zombieId]; // assign local myZombie to storage (on the blockchain)
+    _targetDna = _targetDna % dnaModulus; // make sure that _targetDna isn't longer than 16 digits
+    uint newDna = (myZombie.dna + _targetDna) / 2; // take the average using the .dna from the struct
+    _createZombie("NoName", newDna); // create a new Zombie with name NoName
+  }
 }
